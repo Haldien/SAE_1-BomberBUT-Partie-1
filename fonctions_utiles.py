bd7a3ae5d7c8b5940fcbbe026ca03f13b4254bcb
Haldien <48862782+Haldien@users.noreply.github.com>
2024-12-10 00:18:19 +0100

    Update fonctions_utiles.py

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,4 +1,5 @@
 
+# Valables pour le bomberman et les fantomes mais pas pour les bombes
 def case_valide(grille, y: int, x: int) -> bool:
     if not (0 <= y <= len(grille) - 1 and 0 <= x <= len(grille[0]) - 1):
         return False
