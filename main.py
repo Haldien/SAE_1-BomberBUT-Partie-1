3721d5963de8fb1fb73535a12f285a9a69742f17
Haldien <48862782+Haldien@users.noreply.github.com>
2024-12-09 14:47:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ Fonctions utiles
 def case_valide(grille, y: int, x: int) -> bool:
     if not (0 <= y <= len(grille) - 1 and 0 <= x <= len(grille[0]) - 1):
         return False
-    if grille[y][x] in [["M"], ["C"], ["E"], ["F"]]:
-        return False
+    for el in ["M", "C", "E", "F", "P"]:
+        if el in grille[y][x]:
+            return False
     return True
+
 
 
 """
