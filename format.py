83e112607d2dc103d46d42a45096eaeba99a5326
ShiArtDark <thguichot@gmail.com>
2024-12-09 13:54:40 +0100

    Update format.py

--- format.py
+++ format.py
@@ -3,6 +3,12 @@ def createMap(nomMap:str) -> list:
     """
         Cette fonction prend en argument un nom de map sans l'extension
         Elle reformate cette map pour notre programme
+
+        On cherche à formater tel qu'une liste (la map entière) contienne une liste (pour chaque ligne) qui contient également une liste (pour chaque case)
+        Ce système sert à prévenir des objets pouvant être plusieurs sur une même case.
+        EX : Un joueur ou un fantôme peuvent être sur la même case
+        [" "] -> [ "Bombe" ] -> [ "Bombe", "Fantôme ]
+        
         Elle renvoie ce nouveau formatage.
     """
     mapText = open(f"{nomMap}.txt", "r")
