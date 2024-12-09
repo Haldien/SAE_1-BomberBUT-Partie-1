e4fb6605f8d2152ad811999f847f15db49e7ac9c
Haldien <48862782+Haldien@users.noreply.github.com>
2024-12-10 00:14:53 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -2,23 +2,27 @@ import tkiteasy
 
 from main import *
 
-#  PV : pv du bomber, NIV : niveau du bomber
 
 g = tkiteasy.ouvrirFenetre(400, 400)  # taille à changer
 
 # grille : list[list[list[str]]]
 grille = [
     [["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"]],
-    [["C"], [   ], [   ], ["P"], [   ], [   ], [   ], [   ], ["C"]],
+    [["C"], [], [], ["P"], [], [], [], [], ["C"]],
     [["C"], ["M"], ["C"], ["M"], ["C"], ["M"], ["C"], ["M"], ["C"]],
-    [["C"], ["M"], ["M"], ["M"], ["M"], [   ], ["E"], [   ], ["C"]],
+    [["C"], ["M"], ["M"], ["M"], ["M"], [], ["E"], [], ["C"]],
     [["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"]]
 ]
 
 dic_bombes = {}
 
-timer_global = 20 # à voir
+dic_bomber = {
+    "PV":3,
+    "Niv":1,
+    "Score":0
+}
+
+# timer global = 100; comment gérer sans 'global' ?
 
 
-
-main(grille, g, dic_bombes)
+main(grille, g, dic_bombes, dic_bomber)
