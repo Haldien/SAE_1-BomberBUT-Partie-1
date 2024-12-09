a7696e1f8b37f644d0f04966d9f231f7caf94e93
Haldien <48862782+Haldien@users.noreply.github.com>
2024-12-09 13:06:17 +0100

    Add files via upload

---
+++ bomberman_main.py
@@ -0,0 +1,145 @@
+import tkiteasy
+
+#  PV : pv du bomber, NIV : niveau fdu bomber, pos_bomber : list[int], [y_bomber, x_bomber], soit : y_bomber = indice de ligne, x_bomber : indice de colonne
+global PV, niv, pos_bomber
+
+g = tkiteasy.ouvrirFenetre(400, 400)  # taille à changer
+
+# grille est une liste, pas besoin de global
+# grille : list[list[list[str]]]
+grille = [
+    [["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"]],
+    [["C"], [   ], [   ], ["P"], [   ], [   ], [   ], [   ], ["C"]],
+    [["C"], ["M"], ["C"], ["M"], ["C"], ["M"], ["C"], ["M"], ["C"]],
+    [["C"], ["M"], ["M"], ["M"], ["M"], [   ], ["E"], [   ], ["C"]],
+    [["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"]]
+]
+
+# Fonctions annexes
+
+def case_valide(y: int, x: int) -> bool:
+    if not (0 <= y <= len(grille) - 1 and 0 <= x <= len(grille[0]) - 1):
+        return False
+    if grille[y][x] in [["M"], ["C"], ["E"], ["F"]]:
+        return False
+    return True
+
+
+def deplacer_bomber(y: int, x: int):
+
+    global pos_bomber
+
+    if grille[y][x] == "U":
+        pass
+
+    grille[pos_bomber[0]][pos_bomber[1]].remove("P")
+    grille[y][x].append("P")
+
+    pos_bomber = [y, x]
+
+def poser_bombe(y:int, x:int):
+    if "B" not in grille[y][x] :
+        grille[y][x].append("B")
+        # créer entité bombe avec son timer
+
+
+# Fonctions principales
+
+def action_bomber(touche: str):
+
+    global pos_bomber
+
+    # Mappings des touches vers les vecteurs de mouvements correspondant
+    mouvements = {
+        "z": [-1, 0],
+        "q": [0, -1],
+        "s": [1, 0],
+        "d": [0, 1]
+    }
+
+    # Mouvements
+    if touche in ["z", "q", "s", "d"] and case_valide(pos_bomber[0] + mouvements[touche][0], pos_bomber[1] + mouvements[touche][1]):
+
+        # Jusqu'à ce que le mouvement correspondant soit valide
+
+        deplacer_bomber(pos_bomber[0] + mouvements[touche][0], pos_bomber[1] + mouvements[touche][1])
+
+
+    # Dépose un bombe
+    elif touche == "space":
+        poser_bombe(pos_bomber[0], pos_bomber[1])
+
+    # Passe son tour
+    elif touche == "Return":
+        pass
+
+
+def resoudre_action():
+    pass
+
+
+def deplacer_fantomes():
+    pass
+
+
+def attaque_fantomes():
+    pass
+
+
+def faire_apparaitre_fantomes():
+    pass
+
+
+def updater_timers():
+    pass
+
+
+def explosions():
+    pass
+
+
+def main():
+
+    global pos_bomber
+    for y in range(len(grille)):
+        for x in range(len(grille[0])):
+            if "P" in grille[y][x] :
+                pos_bomber = [y, x]
+
+
+    while True:
+
+        # affichage de la grille dans le terminal pour test
+        print("-----------------------------")
+        for el in grille:
+            print(el)
+
+        touche = g.attendreTouche()
+
+        if touche in ["z", "q", "s", "d", "space", "Return"]:
+            """
+            1. décision de l'action du bomber
+            2. résolution de l'action
+            3. déplacement des fantômes
+            4. attaque des fantômes
+            5. apparition de nouveaux fantômes
+            6. réduction des timers et les explosions
+            """
+            action_bomber(touche)
+
+            resoudre_action()
+
+            deplacer_fantomes()
+
+            attaque_fantomes()
+
+            faire_apparaitre_fantomes()
+
+            updater_timers()
+
+            explosions()
+
+            # render()
+
+
+main()
