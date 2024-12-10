7dbfe8fa1ed84bb1ce5f076b9842e31143f0bcfc
ShiArtDark <thguichot@gmail.com>
2024-12-10 23:07:24 +0100

    Update timers.py

--- timers.py
+++ timers.py
@@ -1,3 +1,13 @@
 def updater_timers_bombes(dic_bombes):
     for key in dic_bombes.keys():
         dic_bombes[key] -= 1
+
+def round_timer(gameSetting:dict) -> None:
+    """
+        Cette fonction prend en paramètre un dictionnaire qui répertorie 
+        les détails du "scénario" du niveau.
+
+        Cette fonction doit être appelé à chaque boucle pour actualiser le tour.
+    """
+    gameSetting['timer'] -= 1
+    gameSetting['timerfantome'] -= 1
