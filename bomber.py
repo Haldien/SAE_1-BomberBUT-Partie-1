e1f7318bc9829faa38af21176ef9d45dd889318e
Haldien <48862782+Haldien@users.noreply.github.com>
2024-12-11 11:00:58 +0100

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -1,15 +1,38 @@
 
 
+
+
 def pos_bomber(grille:list[list[list[str]]])->list[int]:
     for y in range(len(grille)):
         for x in range(len(grille[0])):
             if "P" in grille[y][x]:
                 return [y, x]
 
-def deplacer_bomber(grille, y: int, x: int):
+def deplacer_bomber(grille, y: int, x: int, dic_jeu):
 
-    if grille[y][x] == "U":
-        pass # à voir
+    if "U" in grille[y][x]:
+        grille[y][x].remove("U")
 
-    grille[pos_bomber(grille)[0]][pos_bomber(grille)[1]].remove("P")
+        #on limite le niveau à 4
+        if dic_jeu["bomber"]["Niv"] < 4:
+            level_up(dic_jeu)
+
+
+    grille[dic_jeu["bomber"]["pos"][0]][dic_jeu["bomber"]["pos"][1]].remove("P")
     grille[y][x].append("P")
+
+    dic_jeu["bomber"]["pos"] = (y ,x)
+
+def level_up(dic_jeu):
+
+    dic_jeu["bomber"]["Niv"] += 1
+
+    if dic_jeu["bomber"]["Niv"] in [1, 3]:
+        dic_jeu["bomber"]["PV"] += 1
+
+
+
+
+
+
+
