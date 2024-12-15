600e74acb5508f84ac5d7a0b1e3c811ec501929a
ShiArtDark <thguichot@gmail.com>
2024-12-15 19:11:36 +0100

    Update affiche.py

--- affiche.py
+++ affiche.py
@@ -10,20 +10,18 @@ WIDTH, HEIGHT = 1200, 800
 """
 class Entity:
     def __init__(self, g:object, sprite:dict, size:tuple, y:int,x:int):
-        self.x, self.y = x,y +100
+        self.x, self.y = x,y
         self.sprite = sprite
         self.size = size
         self.g = g
         self.state = "bas"
         self.indexState = 0
+        #self.obj = self.g.dessinerRectangle(self.x, self.y, self.size[1], self.size[0], "red")
         self.obj = self.g.afficherImage(self.x, self.y, self.size , sprite["bas"][self.indexState] )
 
-    def ChangeSprite(self,model:int):
-        match model:
-            case 0: 
-                self.Model_Choice("bas")
-            case 1: 
-                self.Model_Choice("droite")
+    def ChangeSprite(self,direct: str):
+        self.Model_Choice(direct)
+
 
     def Model_Choice(self, state:str) -> None:
         self.g.supprimer(self.obj)
@@ -32,6 +30,10 @@ class Entity:
             self.indexState = 0
 
         self.obj = self.g.afficherImage( self.x, self.y, self.size, self.sprite[state][self.indexState])
+    
+    def supprimerEntite(self):
+        self.g.supprimer(self.obj)
+        del self
 
 """
 
@@ -44,14 +46,19 @@ class Mob(Entity):
     def __init__(self, g:object, sprite:dict, size:tuple, x:int,y:int):
         super().__init__(g, sprite, size, x,y )
 
-    def deplacer(self, coords:tuple[int,int]) -> None:
+    def deplacer(self, coords:tuple[int,int], direct:str) -> None:
         # Changement de sprite
+        self.x = coords[1]*self.size[0]
+        self.y = coords[0]*self.size[1]
+        print(self.x, self.y)
         self.g.deplacer(self.obj, coords[0], coords[1])
-    
-        def subitDegat(self):
-            self.g.supprimer(self.obj)
-            self.obj = self.g.afficherImage
-    
+        self.ChangeSprite(direct)
+
+
+    def subitDegat(self):
+        self.g.supprimer(self.obj)
+        self.obj = self.g.afficherImage
+
 
 class Ethernet(Entity):
     def __init__(self, g:object, sprite:dict, size:tuple, x:int,y:int):
@@ -88,14 +95,7 @@ class Fantome(Mob):
     def __init__(self, g:object, sprite:dict, size:tuple, x:int,y:int):
         super().__init__(g, sprite, size, x,y )
 
-def affiche_plateau(g, grille:list[list[list]]):
-    for x in range(len(grille)):
-        for y in range(len(grille[0])):
-            if grille[x][y] == ['C']:
-                g.dessinerRectangle(y*CASE,100+x*CASE, CASE, CASE, "white")
-
-
-
+"""
 
 
 
@@ -109,6 +109,7 @@ grille = [
     [["C"], ["U"], [   ], ["M"], ["M"], ["M"], [   ], ["U"], ["C"]],
     [["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"], ["C"]]
 ]
+
 
 coord = bomber.pos_bomber(grille)
 
@@ -126,6 +127,7 @@ bunny = {
     "up": ["asset/bunny/up/up.png"]
 }
 
+"""
 """
 dict de sprite
 
@@ -147,27 +149,4 @@ clé dict Sprite / clé entité / direction / image
 """
 
 
-new = Bomber(g, bunny, (CASE,CASE), coord[0]*CASE, coord[1]*CASE)
-
-for i in range(5):
-    g.update()
-    time.sleep(.5)
-    new.ChangeSprite(1)
-    g.update()
-    time.sleep(.5)
-
-for i in range(5):
-    g.update()
-    time.sleep(.5)
-    new.ChangeSprite(0)
-    g.update()
-    time.sleep(.5)
-
-
-
-while not g.recupererClic():
-
-    pass
-
-g.fermerFenetre()
 
