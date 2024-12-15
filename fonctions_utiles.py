d7e30f31246fcb90041635fabf19efbf80528058
ShiArtDark <thguichot@gmail.com>
2024-12-15 19:13:01 +0100

    Update fonctions_utiles.py

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -53,22 +53,63 @@ def affichage_grille(grille):
                 print(f"{case}", end = " ")
         print("\n")
 
-def get_object_pos(grille:list, cible:str) -> list:
+def get_param( nomMap:str) -> tuple[int,int]:
     """
-    Cette fonction permet de chercher un objet particulier dans une grille
-    str cible 
-        - P, le bomber  
-        - E, la prise Ethernet -
-        - M, Mur
-        - C, une colonne
-        - F, si des fantômes sont déjà existant dans le bâtiment Bastier
-    """
-    recherche = []
-    for x in range(len(grille)):
-        for y in range(len(grille[0])):
-            for el in grille[x][y]:
-                if cible in el:
-                    recherche += [(x,y)]   
-    return recherche
+        Cette fonction prend en paramètre une nom de map sans l'extension
 
-# Il y a peut être un moyen qu'on fasse une fonction qui récupère tout d'un coup
+        Elle permet de récupérer le scénario d'une map
+        
+        Elle retourne les valeurs de ces paramètres
+    """
+
+    param = open(f"{nomMap}.txt", "r")
+    tmp = param.readlines()[0:2]
+    val = (int(tmp[0].split()[1]), int(tmp[1].split()[1]))
+    param.close()
+    
+    return val
+    
+
+    
+
+def get_scenario(nomMap:str) -> tuple[dict, dict, list[list[list[str]]]]:
+    """
+    Cette fonction permet de générer le dic_jeu qui répertorie tout les objets particulier ainsi que les paramètres du jeu
+    bomber, bombes, ethernet et fantôme.
+    """
+    dic_jeu = {
+        "bomber" :{ 'pos' : None, 'PV': 3, 'Niv': 0, 'Score': 0, 'cooldown' : 0 },
+
+        "fantomes": {},
+        "bombes" : {},
+
+        "ethernet" : { "pos" : [] }
+        
+    }
+
+    settings = {
+        "timer" : 100,
+        "timerfantome": 20,
+        "nombrefantome": 0
+    }
+
+    param = get_param(nomMap)
+    settings['timer'], settings['timerfantome'] = param[0], param[1]
+
+    grille = create_map(nomMap)
+    for y in range(len(grille)):
+        for x in range(len(grille[0])):
+            if "P" in grille[y][x]:
+                dic_jeu["bomber"]["pos"] = (y,x)
+            elif "E" in grille[y][x]:
+                dic_jeu["ethernet"]["pos"] += [(y,x)]
+            elif "F" in grille[y][x]:
+                grille[y][x][0] += str(settings["nombrefantome"])
+                dic_jeu["fantomes"] = {f"F{settings["nombrefantome"]}" : ["ObjetGraphique", (y,x)]}
+                settings["nombrefantome"] += 1
+
+    
+    return (dic_jeu, settings, grille)
+
+
+    
