82b02aac2396e780dbcfc0c6948f31413cdb230f
ShiArtDark <thguichot@gmail.com>
2024-12-17 12:20:45 +0100

    Update timers.py

--- timers.py
+++ timers.py
@@ -1,7 +1,8 @@
 def updater_timers_bombes(dic_jeu):
     for key in dic_jeu["bombes"].keys():
-        dic_jeu["bombes"][key] -= 1
-
+        dic_jeu["bombes"][key]["timer"] -= 1
+        dic_jeu["bombes"][key]["obj"].ignite(dic_jeu["bombes"][key]["timer"])
+        
 def round_timer(gameSetting:dict) -> None:
     """
         Cette fonction prend en paramètre un dictionnaire qui répertorie 
