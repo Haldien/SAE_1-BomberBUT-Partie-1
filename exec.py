1272d72325b20d0d45ad9f0fd2069051f00f82cf
ShiArtDark <thguichot@gmail.com>
2024-12-20 13:27:21 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -2,13 +2,6 @@ import tkiteasy
 
 from main import *
 
-
-grille_dimensions = (21, 21)  # !! Y, X
-
-fenetre_dimensions = (800, 600)  # !! X, Y (1280, 960)
-
-
-
 g = tkiteasy.ouvrirFenetre(fenetre_dimensions[0], fenetre_dimensions[1])
 
 
